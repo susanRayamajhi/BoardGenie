f02a7e4b74758baabb3618fb2482d88159a10849
Susan Rayamajhi <sushanraya2002@gmail.com>
2026-04-17 11:22:40 +0100

    updated some sources on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -36,13 +36,13 @@ function App() {
           <ul>
             <li>
               <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
+                <img className="logo" alt="" />
                 Explore Vite
               </a>
             </li>
             <li>
               <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
+                <img className="button-icon" alt="" />
                 Learn more
               </a>
             </li>
